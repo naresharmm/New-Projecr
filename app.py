20c15ffee2faf79f9ec895a1a052cc1d813dfaa5
vxsoft <sharmazanovanare@gmail.com>
2024-01-16 12:26:27 +0400

    second commit

---
+++ app.py
@@ -0,0 +1,13 @@
+''' Flask project
+'''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8080)
