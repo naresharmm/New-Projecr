539d567ae35082de5bae81b0f7f029f14859380a
naresharmm <sharmazanovanare@gmail.com>
2024-02-26 20:56:46 +0400

    removed session/request from user_controller

--- user_controller.py
+++ user_controller.py
@@ -1,33 +1,15 @@
 import sqlite3
 from cryptography.fernet import Fernet
 
-from flask import session 
-
 from user import UserValidator
 
-
 class UserController:
-    def __init__(self, session: dict, request) -> None:
-        self.session = session
-        self.request = request
-        self.cipher_suite =\
-        Fernet(b'DHML65d-nY3iZL1vsWkrmzf2kSfoHQ9Fnv6IWlyIPzQ=')
-        
-    def logout(self) -> bool:
-        """
-        Logout the current user.
-        Returns
-        -------
-        bool
-            True if logout is successful.
-        """
-        session.clear()
-        return True
+    def __init__(self) -> None:
+        self.cipher_suite = Fernet(b'DHML65d-nY3iZL1vsWkrmzf2kSfoHQ9Fnv6IWlyIPzQ=')
 
-    def register(self, form_data: dict) -> bool:
+    def register(self, form_data: dict, session: dict) -> bool:
         password_form = form_data.get("password")
-        encrypted_password = \
-        self.cipher_suite.encrypt(password_form.encode()).decode()
+        encrypted_password = self.cipher_suite.encrypt(password_form.encode()).decode()
         if UserValidator().validate_registration(form_data):
             try:
                 conn = sqlite3.connect('app.db')
@@ -36,18 +18,15 @@ class UserController:
                     INSERT INTO users (phone_number, email, password, node_ids)
                     VALUES (?, ?, ?, ?)
                 ''',
-                (form_data['phone_number'],\
-                 form_data['email'], encrypted_password, ''))
-
+                (form_data['phone_number'], form_data['email'], encrypted_password, ''))
                 conn.commit()
 
-                cursor.execute('SELECT id FROM users WHERE\
-                phone_number = ?', (form_data['phone_number'],))
+                cursor.execute('SELECT id FROM users WHERE phone_number = ?', (form_data['phone_number'],))
                 user_id = cursor.fetchone()[0]
 
                 conn.close()
 
-                self.session["user_id"] = user_id 
+                session["user_id"] = user_id
                 return True
 
             except Exception as e:
@@ -56,23 +35,20 @@ class UserController:
         else:
             return False
 
-
-    def login(self, phone_number: str, password: str) -> bool:
+    def login(self, phone_number: str, password: str, session: dict) -> bool:
         try:
             conn = sqlite3.connect('app.db')
             cursor = conn.cursor()
 
-            cursor.execute('SELECT id, \
-            password FROM users WHERE phone_number = ?', (phone_number,))
+            cursor.execute('SELECT id, password FROM users WHERE phone_number = ?', (phone_number,))
             user_data = cursor.fetchone()
             conn.close()
 
             if user_data:
                 user_id, hashed_password = user_data
-                dec_password =\
-                self.cipher_suite.decrypt(hashed_password.encode()).decode()
+                dec_password = self.cipher_suite.decrypt(hashed_password.encode()).decode()
                 if dec_password == password:
-                    self.session["user_id"] = user_id
+                    session["user_id"] = user_id
                     return True
 
         except Exception as e:
@@ -80,8 +56,8 @@ class UserController:
 
         return False
 
-    def get_profile(self) -> dict:
-        user_id = self.session.get("user_id")
+    def get_profile(self, session: dict) -> dict:
+        user_id = session.get("user_id")
         if user_id:
             nodes = {}
             try:
--- views.py
+++ views.py
@@ -16,165 +16,66 @@ from user_controller import UserController
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 
+user_controller = UserController()
+
 @app.route('/')
 def home() -> str:
-    """
-    Render the home page.
-
-    Returns:
-        str: Rendered HTML content for the home page.
-    """
     countries = get_countries()
     return render_template('home.html', countries=countries)
 
 @app.route('/profile')
 def profile() -> str:
-    """
-    Render the user profile page.
-
-    Returns
-    ------------------------
-        str: 
-        Rendered HTML content for the user profile page.
-    """
-    user_controller = UserController(session, request)
-    nodes = user_controller.get_profile()
+    nodes = user_controller.get_profile(session)
     return render_template('profile.html', nodes=nodes)
-
 
 @app.route('/register', methods=['GET'])
 def register_form() -> str:
-    """
-    Render the registration form.
-
-    Returns
-    ------------------------
-        str: 
-        Rendered HTML content for the registration form.
-    """
-    return render_template('registration.html', \
-    countries=get_countries())
+    return render_template('registration.html', countries=get_countries())
 
 @app.route('/register', methods=['POST'])
 def register() -> str:
-    """
-    Handle user registration form submission.
-
-    Returns
-    ------------------------
-        str: 
-    """
-    user_controller = UserController(session, request)
-    if user_controller.register(request.form):
+    if user_controller.register(request.form, session):
         return redirect(url_for('profile'))
     else:
         return "Data inputted wrong"
 
 @app.route('/login', methods=['GET'])
 def login_form() -> str:
-    """
-    Render the login form.
-
-    Returns
-    ------------------------
-        str: Rendered HTML content for the login form.
-    """
     return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
 def login() -> str:
-    """
-    Handle user login form submission.
-
-    Returns
-    ------------------------
-        str:
-        Either redirects to the profile page
-        or sdisplays an error message.
-    """
-    user_controller = UserController(session, request)
-    if user_controller.login(request.form.get('phone_number'),\
-     request.form.get('password')):
+    if user_controller.login(request.form.get('phone_number'), request.form.get('password'), session):
         return redirect(url_for('profile'))
     else:
         return "Phone number or password is incorrect"
 
 @app.route('/logout', methods=['GET'])
 def logout() -> str:
-    """
-    Handle user logout.
-
-    Returns
-    ------------------------
-        str: Redirects to the home page after logging out.
-    """
-    user_controller = UserController(session, request)
-    user_controller.logout()
+    session.clear()
     return redirect(url_for('home'))
 
 @app.route('/save_text', methods=['POST'])
 def save_text() -> str:
-    """
-    Save text data.
-
-    Returns
-    ------------------------
-        str: JSON response indicating success or failure.
-    """
     data_controller = DataController(session, request)
     request_data = request.get_json()
-    response, status_code = NodesController().save_text(
-        text_data=request_data,
-        session=session
-    )
+    response, status_code = NodesController().save_text(text_data=request_data, session=session)
     return jsonify(response), status_code
 
 @app.route('/get_saved_texts')
 def get_saved_texts() -> str:
-    """
-    Get saved text data.
-
-    Returns
-    ------------------------
-        str: JSON response containing saved text data.
-    """
     data_controller = DataController(session, request)
     response, status_code = NodesController.get_saved_texts(session)
     return jsonify(response), status_code
 
 @app.route('/profile/delete_text/<node_id>', methods=['GET', 'POST'])
 def delete_text(node_id: str) -> str:
-    """
-    Delete text data.
-
-    Parameters
-    ------------------------
-        node_id (str): 
-
-    Returns
-    ------------------------
-        str:
-        
-    """
     data_controller = DataController(session, request)
     response, status_code = data_controller.delete_text(node_id)
     return jsonify(response), status_code
 
 @app.route('/profile/edit_text/<node_id>', methods=['POST'])
 def edit_text(node_id: str) -> str:
-    """
-    Edit text data.
-
-    Parameters
-    ------------------------
-        node_id (str): 
-        The ID of the text node to edit.
-
-    Returns
-    ------------------------
-        str: 
-       
-    """
     data_controller = DataController(session, request)
     response, status_code = data_controller.edit_text(node_id)
     return jsonify(response), status_code
